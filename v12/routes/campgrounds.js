2517665b9ea68c4b8225e8871a4978b0eceb27f5
Apollo_Brainiac <AxMPerez@gmail.com>
2017-07-19 14:33:23 +0000

    update create and edit campground routes

--- v12/routes/campgrounds.js
+++ v12/routes/campgrounds.js
@@ -19,14 +19,14 @@ router.get("/", function(req, res){
 // CREATE -- ADD NEW CAMPGROUNDS
 router.post("/", middleware.isLoggedIn, function(req, res){
   var name = req.body.name;
-  var price = req.body.price;
   var image = req.body.image;
+  var cost = req.body.cost;
   var desc = req.body.description;
   var author = {
     id: req.user._id,
     username: req.user.username
   };
-  var newCampground = {name: name,price: price, image: image, description: desc, author:author};
+  var newCampground = {name: name, image: image, cost: cost, description: desc, author: author};
   // Create a new campground and save to DB
   Campground.create(newCampground, function(err, newlyCreated){
     if(err){
